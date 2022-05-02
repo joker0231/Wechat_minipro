782b67c4c11d352b457bc401b4be6ec534e1005a
Ekko306 <1186179435@qq.com>
2022-05-02 22:05:58 +0800

    结构调整

---
+++ miniprogram/pages/login/register/index.js
@@ -0,0 +1,77 @@
+// pages/register/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     register_student:"register_show",
+     register_teacher:"register_hide"
+  },
+
+  changetostudent: function (){
+      this.setData({
+        register_student:"register_show",
+        register_teacher:"register_hide"})
+  },
+
+  changetoteacher: function (){
+      this.setData({
+        register_student:"register_hide",
+        register_teacher:"register_show"})
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
--- miniprogram/pages/mine/index/index.js
+++ miniprogram/pages/mine/index/index.js
@@ -2,5 +2,17 @@ Page({
     data: {},
     onLoad: function (options) {
 
+    },
+
+    clickCheckIn: function() {
+        wx.navigateTo({
+          url: '/pages/mine/checkIn/index',
+        })
+    },
+
+    clickLogout: function() {
+        wx.reLaunch({
+          url: '/pages/login/index/index',
+        })
     }
 });
