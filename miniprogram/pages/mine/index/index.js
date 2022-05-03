6e72ed6c29d6ab00c75f012a5e2a1620c2b98094
Ekko306 <1186179435@qq.com>
2022-05-03 14:49:58 +0800

    增加可视化进度,添加vant的npm构建与libs eccharts

--- miniprogram/pages/mine/index/index.js
+++ miniprogram/pages/mine/index/index.js
@@ -5,14 +5,20 @@ Page({
     },
 
     clickCheckIn: function() {
-        wx.navigateTo({
-          url: '/pages/mine/checkIn/index',
-        })
+      wx.navigateTo({
+        url: '/pages/mine/checkIn/index',
+      })
     },
 
     clickLogout: function() {
-        wx.reLaunch({
-          url: '/pages/login/index/index',
-        })
+      wx.reLaunch({
+        url: '/pages/login/index/index',
+      })
+    },
+
+    clickProgress: function() {
+      wx.navigateTo({
+        url: '/pages/mine/progress/index',
+      })
     }
 });
