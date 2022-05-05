ef5e523e6047b983576f37696abece14a27a1e10
苏亚鹏 <1632455649@qq.com>
2022-05-05 23:59:15 +0800

    update

---
+++ miniprogram/pages/mine/edit/index.js
@@ -0,0 +1,35 @@
+Page({
+    data: {
+        name:{
+            type:String,
+            value:"crystal"
+        },
+        sex:{
+            type:String,
+            value:"女"
+        },
+        school:{
+            type:String,
+            value:"武汉中学"
+        },
+        grade:{
+            type:String,
+            value:"高三"
+        },
+        classes:{
+            type:Number,
+            value: 4
+        },
+        phone:{
+            type:Number,
+            value: 18702745270
+        },
+        src:{
+            type:String,
+            value:"https://6c65-lemon-7glhwqyu5304e1f9-1311072024.tcb.qcloud.la/logo.png?sign=6439095f87d6cb1a7f5abe0d045ac9c6&t=1651722239"
+        }
+    },
+    onLoad: function (options) {
+
+    }
+});
